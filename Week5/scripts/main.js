058da5e8b5184bbf88bcea3070c8e23273a0802e
Faizan Ahmed <5ahmef94@solent.ac.uk>
2022-03-03 15:31:21 +0000

    This is the JavaScript file used the tasks in Week 5.

---
+++ Week5/scripts/main.js
@@ -0,0 +1,9 @@
+
+
+
+document.getElementById("BtnCloseBanner").addEventListener("click", e=>{
+    document.getElementById("cookieBanner").style.display = "none";
+    /* 'e' is a parameter which refers to an event, and in this case it's
+    referring to the click event that happens when a user clicks on the X
+    button for the banner.*/
+});
