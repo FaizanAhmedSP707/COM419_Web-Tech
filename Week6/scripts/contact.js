9cf5b0679d1c83f8b9f3a492873f01ec5f885091
Faizan Ahmed <5ahmef94@solent.ac.uk>
2022-03-14 13:33:25 +0000

    Uploaded the initial set-up of the actual files for the week, more to come soon. (WORK STILL IN PROGRESS)

---
+++ Week6/scripts/contact.js
@@ -0,0 +1,21 @@
+/* 
+'e' is a parameter which refers to an event, and in this case it's
+referring to the click event that happens when a user clicks on the X
+button for the banner.
+
+'e=>{}' is an arrow function, a much faster and a short hand way of writing functions
+in JavaScript.
+*/
+
+
+document.getElementById("BtnCookieBanner").addEventListener("click", e=>{
+    document.getElementById("cookieBanner").style.display = "none";
+    
+    /*This is the JavaScript part of the annoying cookie banner.*/
+});
+
+
+/*
+document.getElementById("queryUserQn").style.display = "none";
+document.getElementById("queryUserQn").style.display = "block";
+*/
